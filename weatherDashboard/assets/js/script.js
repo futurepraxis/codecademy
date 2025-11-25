f48f8c73b29dade2b6d4dca869295889eac2432d
futurepraxis <61433938+futurepraxis@users.noreply.github.com>
2025-11-25 11:39:24 -0600

    created function to render 5-day forecast

--- weatherDashboard/assets/js/script.js
+++ weatherDashboard/assets/js/script.js
@@ -7,8 +7,6 @@ const GEO_URL = 'http://api.openweathermap.org/geo/1.0/direct';
 //Hooks
 const searchForm = document.getElementById('searchForm');
 const cityInput = document.getElementById('cityInput');
-let currentWeather = null;
-let forecastWeather = null;
 
 //Add event handler to submit form
 searchForm.addEventListener("submit", (e) => {
@@ -17,7 +15,6 @@ searchForm.addEventListener("submit", (e) => {
     let isAlphabetic = /^[A-Za-z]+$/.test(city);
     if (isAlphabetic) {
         getGeolocation(city);
-        //getForecastWeather();
     } else {
         alert("Please enter a valid city (A-Z only)");
     };
@@ -42,6 +39,7 @@ async function getGeolocation(city) {
           let lat = data[0].lat;
           let lon = data[0].lon;
           getCurrentWeather(lat, lon);
+          getForecastWeather(lat, lon);
       }
   } catch (error) {
     console.error(error.message);
@@ -56,35 +54,86 @@ function getCurrentWeather(lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      currentWeather = data;
-      updateCurrentWeatherUI();
+      updateCurrentWeatherUI(data);
+    });
+};
+
+//Look up 5-day forecast data
+function getForecastWeather(lat, lon) {
+    const url = `${FORECAST_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
+    fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      updateForecastWeatherUI(data);
     });
 };
 
 //render current weather card
-function updateCurrentWeatherUI() {
-    const cityName = currentWeather.name;
+function updateCurrentWeatherUI(data) {
+    document.getElementById('weatherInfo').classList.toggle('is-hidden');
+    const cityName = data.name;
     const currentWeatherText = document.getElementById('currentWeatherDetails');
-    const currentTemp = Math.round(currentWeather.main.temp);
-    const currentFeelsLike = Math.round(currentWeather.main.feels_like);
-    const currentWind = Math.round(currentWeather.wind.speed);
-    const icon = currentWeather.weather[0].icon;
+    const currentTemp = Math.round(data.main.temp);
+    const currentFeelsLike = Math.round(data.main.feels_like);
+    const currentWind = Math.round(data.wind.speed);
+    const icon = data.weather[0].icon;
     const iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-    // document.getElementById('weatherInfo').classList.toggle('is-hidden');
     document.getElementById('currentCityName').innerHTML = `<strong>Current City:</strong> ${cityName}`;
     document.getElementById('currentTemp').textContent = currentTemp;
     document.getElementById('currentIcon').src = iconURL;
-    document.getElementById('currentDescription').textContent = currentWeather.weather[0].description;
+    document.getElementById('currentDescription').textContent = data.weather[0].description;
     
     currentWeatherText.innerHTML =  `
     <li><strong>Feels Like:</strong> ${currentFeelsLike}°F</li>
-    <li><strong>Current Humidity:</strong> ${currentWeather.main.humidity}%</li>
+    <li><strong>Current Humidity:</strong> ${data.main.humidity}%</li>
     <li><strong>Current Wind:</strong> ${currentWind} mph</li>
    `
-}
+};
+
+//render forecast cards 
+function updateForecastWeatherUI(data) {
+    // Select one forecast per day
+    const daily = data.list.filter((item) =>
+        item.dt_txt.includes('12:00:00')
+    );
+
+    daily.forEach((item) => {
+    const date = new Date(item.dt_txt);
+    const {
+      main: { temp },
+      weather,
+    } = item;
+    
+    const formattedDate = date.toLocaleDateString(undefined, {
+      weekday: 'short',
+      month: 'short',
+      day: 'numeric',
+    });
+    const icon = weather[0].icon;
+    const iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    const forecastGrid = document.getElementById('forecastGrid');
+    const forecastCard = document.createElement('div');
+    forecastCard.classList.add('cell');
+    forecastCard.innerHTML = `
+        <div class="card content has-text-centered p-3">
+            <h4 class="is-size-4">${formattedDate}</h4>
+                <div class="card-image">
+                    <figure class="image is-96x96 mx-auto">
+                            <img src="${iconURL}" alt="" />
+                    </figure>
+                </div>
+            <h5 class="is-size-5 has-text-centered is-capitalized">${Math.round(temp)}°F</h4>
+            <p class="is-capitalized">${weather[0].description}</p>
+        </div>
+    `;
+    forecastGrid.appendChild(forecastCard);
+  });
+};
 
 //To-do:
-// get forecast data
+//figure out way to accept city names with spaces (e.g., New York)
 // add recent cities list
 // think about better ways to render the UI and add error messages
 // add date?
